434d19325af38a1811a7834bc6f6592f8ce234bb
Alejandro Suárez Perales <62011966+asu4net@users.noreply.github.com>
2023-04-25 18:15:33 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "rttr"
+    kind "StaticLib"
+    language "C++"
+    
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    targetdir ("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/Intermediate/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/rttr/**.h",
+        "src/rttr/**.cpp"
+    }
+
+    includedirs
+    {
+        "src/rttr"
+    }
+
+    defines 
+    {
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+    
+    filter "configurations:Debug"
+        defines "NIT_DEBUG"
+        runtime "Debug"
+        symbols "on"
+    
+    filter "configurations:Release"
+        defines "NIT_RELEASE"
+        runtime "Release"
+        optimize "on"
